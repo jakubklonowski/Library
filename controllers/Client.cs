01e2b9039b4eedfbb439630433f1abea0247561e
Jakub Klonowski <67651404+kapitanjaszczur@users.noreply.github.com>
2023-05-15 13:56:13 +0200

    add Client module

---
+++ controllers/Client.cs
@@ -0,0 +1,126 @@
+﻿using Library.models;
+using Library.repositories;
+using Library.services;
+
+namespace Library
+{
+    public partial class Client : Form
+    {
+        readonly ClientService service;
+        private readonly string connString = "Data Source=;Initial Catalog=;User Id=;Password=";
+
+        public Client()
+        {
+            InitializeComponent();
+            service = new ClientService(new ClientRepository(connString));
+            fetchData();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            string clientName = textBoxClientName.Text;
+            ClientRequest clientRequest = new()
+            {
+                Name = clientName
+            };
+            addClient(clientRequest);
+            clearForms();
+            fetchData();
+        }
+
+        private void buttonModify_Click(object sender, EventArgs e)
+        {
+            string clientId = "";
+
+            try
+            {
+                if (comboBoxClientId.SelectedItem == null)
+                {
+                    throw new ArgumentException();
+                }
+                else
+                {
+                    clientId = comboBoxClientId.SelectedItem.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            string clientName = textBoxClientName.Text;
+            ClientModel client = new()
+            {
+                Id = clientId,
+                Name = clientName
+            };
+            modifyClientData(client);
+            clearForms();
+            fetchData();
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            string clientId = "";
+
+            try
+            {
+                if (comboBoxClientId.SelectedItem == null)
+                {
+                    throw new ArgumentException();
+                }
+                else
+                {
+                    clientId = comboBoxClientId.SelectedItem.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            ClientResponse clientResponse = new()
+            {
+                Id = clientId
+            };
+            deleteClient(clientResponse);
+            clearForms();
+            fetchData();
+        }
+
+        private void fetchData()
+        {
+            // populates dataGridViewClients from database
+            dataGridViewClients.DataSource = service.getClientsData();
+
+            // clears & populates comboBoxClientId from database
+            comboBoxClientId.Items.Clear();
+            List<ClientResponse> list = service.getClientsId();
+            foreach (ClientResponse item in list)
+            {
+                comboBoxClientId.Items.Add(item.Id.ToString());
+            }
+        }
+
+        private void addClient(ClientRequest clientRequest)
+        {
+            service.addClient(clientRequest);
+        }
+
+        private void modifyClientData(ClientModel client)
+        {
+            service.modifyClient(client);
+        }
+
+        private void deleteClient(ClientResponse clientResponse)
+        {
+            service.deleteClient(clientResponse);
+        }
+
+        private void clearForms()
+        {
+            comboBoxClientId.SelectedItem = null;
+            textBoxClientName.Clear();
+        }
+    }
+}
---
+++ services/ClientService.cs
@@ -0,0 +1,109 @@
+﻿using Library.models;
+using Library.repositories;
+using Library.services.validators;
+
+namespace Library.services
+{
+    public class ClientService
+    {
+        readonly ClientRepository _repository;
+
+        public ClientService(ClientRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public BindingSource getClientsData()
+        {
+            BindingSource bsource = new();
+            try
+            {
+                bsource = _repository.getClientsData();
+                if (bsource == null || bsource.Count == 0)
+                {
+                    MessageBox.Show("No results!");
+                    throw new ArgumentNullException(nameof(bsource));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return bsource;
+        }
+
+        public List<ClientResponse> getClientsId()
+        {
+            List<ClientResponse> list = new();
+            try
+            {
+                list = _repository.getClientsId();
+                if (list == null || list.Count == 0)
+                {
+                    MessageBox.Show("No results!");
+                    throw new ArgumentNullException(nameof(list));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return list;
+        }
+
+        public void addClient(ClientRequest clientRequest)
+        {
+            try
+            {
+                string name = clientRequest.Name;
+
+                Validators.StringShortOrNull(5, name);
+
+                _repository.addClient(clientRequest);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void modifyClient(ClientModel client)
+        {
+            try
+            {
+                string id = client.Id;
+                string name = client.Name;
+
+                Validators.Atoi(id);
+                Validators.StringShortOrNull(5, name);
+                Validators.StringShortOrNull(1, id);
+                _ = int.TryParse(id, out int intid);
+                Validators.IsNaturalNumber(intid);
+
+                _repository.modifyClient(client);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void deleteClient(ClientResponse clientResponse)
+        {
+            try
+            {
+                string id = clientResponse.Id;
+
+                Validators.Atoi(id);
+                _ = int.TryParse(id, out int intid);
+                Validators.IsNaturalNumber(intid);
+
+                _repository.deleteClient(clientResponse);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
